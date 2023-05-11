aefda2b7b4bfa92c57be0bed44c8798d6fc5445b
masoud arabi <applepie@masouds-MacBook-Air.local>
2023-05-11 17:58:12 -0400

    Add user controller and crud actions

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,45 @@
+class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show update destroy]
+
+  
+   def index
+        @users = User.all
+        render json: UserSerializer.new(@users).serializable_hash
+    end
+
+    def show 
+        render json: UserSerializer.new(@user).serializable_hash 
+    end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save 
+      render json: UserSerializer.new(@user).serializable_hash, status: :created
+    else
+        render json: @user.errors, status: :unprocessable_entity 
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: UserSerializer.new(@user).serializable_hash, status: :ok
+    else
+        render json: @user.errors, status: :unprocessable_entity 
+    end
+  end
+
+  def destroy
+    @user.destroy 
+    head 204
+  end
+
+  private
+
+  def user_params 
+    params.require(:user).permit(:email, :password_digest)
+  end
+
+  def set_user 
+    @user = User.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :users, only: [:index,:show, :create, :update, :destroy]
       resources :airlines, param: :slug
-      resources :reviews, only:[:create, :update, :destroy]
+      resources :reviews, only: [:create, :update, :destroy]
       post '/login', to: 'tokens#login'
     end
   end
